a00bac21e4737a4bb9d0d3bc74c88a35bcab2b56
SANDU POSTARU <kuoacat@gmail.com>
2016-02-22 15:38:29 +0100

    Add First part

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject filmania "0.1.0-SNAPSHOT"
+  :description "Des films, du data, de la science... 
+Du data-science pour les films..."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.csv "0.1.3"]])
+
+
